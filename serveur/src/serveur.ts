5f50b89e3cb0269c15dde9fd3c2485fa89dce16e
EPMPaulPoly <paul.charbonneau@polymtl.ca>
2026-01-16 14:22:57 -0500

    Petites modifs pour permettre l'accès et la fonctionnalité sur ubuntu

--- serveur/src/serveur.ts
+++ serveur/src/serveur.ts
@@ -32,7 +32,8 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
 // Start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
-  console.log(`Server address info:${config.database.database}`)
+  console.log(`Server address info:${config.database.host}`)
+  console.log(`Server database info:${config.database.database}`)
   console.log(`Server running at ${new Date().toISOString()}`); 
 });
 
