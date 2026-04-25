d75fbea50810af7e6b145f13c0838e50c2f72110
EPMPaulPoly <paul.charbonnneau@polymtl.ca>
2026-04-24 20:44:31 -0400

    Créer un statemetn if pour vérifier pour creds http
    petit if statement si la variable d'environnmenet est créée je la transmet comme moyen d'authentification
    sinon je laisse le champs vide

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,16 +1,43 @@
-import axios, { AxiosError, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosError, InternalAxiosRequestConfig, AxiosResponse,AxiosInstance } from 'axios';
 
-const api = axios.create({
-  baseURL:  `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:5000/api', //
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
 
+let api:AxiosInstance;
+if (process.env.REACT_APP_BE_USER && process.env.REACT_APP_BE_PASSWORD) {
+  console.log('env:',process.env);
+  console.log('Backend User:',process.env.REACT_APP_BE_USER,'\nBackend Password:', process.env.REACT_APP_BE_PASSWORD,'\nAPI URL:',process.env.REACT_APP_API_URL);
+  api = axios.create({
+    baseURL:  `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:5000/api', //
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth:{
+      username: process.env.REACT_APP_BE_USER,
+      password: process.env.REACT_APP_BE_PASSWORD
+    }
+  });
+} else{
+  console.log('Backend User:',process.env.REACT_APP_BE_USER,'\n Backend Password:', process.env.REACT_APP_BE_PASSWORD,'\n API URL:',process.env.REACT_APP_API_URL);
+  console.log('env:',process.env);
+  api = axios.create({
+    baseURL:  `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:5000/api', //
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
 // Request interceptor with correct typing
 api.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    console.log('Starting Request:', config.url);
+    const user = process.env.REACT_APP_BE_USER;
+    const password = process.env.REACT_APP_BE_PASSWORD;
+
+    if (user && password) {
+      const token = btoa(`${user}:${password}`);
+
+      config.headers = config.headers ?? {};
+      config.headers.Authorization = `Basic ${token}`;
+    }
+
     return config;
   }
 );
