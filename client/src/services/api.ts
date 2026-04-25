fea2327151317d6b5db3148e7eae904c287f3c24
EPMPaulPoly <paul.charbonnneau@polymtl.ca>
2026-04-25 03:54:30 -0400

    Ménage api.ts
    api.ts était mis en place pour avoir un login pour le backend basé http
    vu que le backend est maintenant basé sur des sessions, plus besoin de compliquer autant le api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,44 +2,24 @@ import axios, { AxiosError, InternalAxiosRequestConfig, AxiosResponse,AxiosInsta
 
 
 let api:AxiosInstance;
-if (process.env.REACT_APP_BE_USER && process.env.REACT_APP_BE_PASSWORD) {
-  console.log('env:',process.env);
-  console.log('Backend User:',process.env.REACT_APP_BE_USER,'\nBackend Password:', process.env.REACT_APP_BE_PASSWORD,'\nAPI URL:',process.env.REACT_APP_API_URL);
-  api = axios.create({
-    baseURL:  `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:5000/api', //
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    auth:{
-      username: process.env.REACT_APP_BE_USER,
-      password: process.env.REACT_APP_BE_PASSWORD
-    }
-  });
-} else{
+
   console.log('Backend User:',process.env.REACT_APP_BE_USER,'\n Backend Password:', process.env.REACT_APP_BE_PASSWORD,'\n API URL:',process.env.REACT_APP_API_URL);
   console.log('env:',process.env);
   api = axios.create({
     baseURL:  `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:5000/api', //
     headers: {
       'Content-Type': 'application/json'
-    }
+    },
+    withCredentials: true,
   });
-}
+
 // Request interceptor with correct typing
 api.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    const user = process.env.REACT_APP_BE_USER;
-    const password = process.env.REACT_APP_BE_PASSWORD;
-
-    if (user && password) {
-      const token = btoa(`${user}:${password}`);
-
-      config.headers = config.headers ?? {};
-      config.headers.Authorization = `Basic ${token}`;
-    }
-
+    console.log('Starting Request:', config.url);
     return config;
   }
+
 );
 
 // Response interceptor with correct typing
