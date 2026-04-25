9a9259f5c0141dcd9d142f68dd0e9f24357b0529
EPMPaulPoly <paul.charbonnneau@polymtl.ca>
2026-04-25 02:56:40 -0400

    Modifications pour faire fonctionner auth Avait besoin de spcéfier une url de base, les sources trustées et le mode d'authenticication par email

--- serveur/src/lib/auth.ts
+++ serveur/src/lib/auth.ts
@@ -4,5 +4,10 @@ import {pool} from "./poolCreate";
 
 export const auth = betterAuth({
     database: pool,
+    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:5000/",
     secret: process.env.BETTER_AUTH_SECRET,
+    emailAndPassword:{
+        enabled:true,
+    },
+    trustedOrigins:["http://localhost:3000"]
 });
