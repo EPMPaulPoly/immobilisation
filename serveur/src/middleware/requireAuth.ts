e3c493f5d21965cb99c3a788d84fa8ae14b1a05a
EPMPaulPoly <paul.charbonnneau@polymtl.ca>
2026-04-25 04:24:25 -0400

    Message erreur authentificatin
    Rendu le message d'authentification plus claire pour les utilisateurs qui tentent d'accéder à l'API indépendemment
    Peut utiliser le plugin de betterauth pour générer des clefs.

--- serveur/src/middleware/requireAuth.ts
+++ serveur/src/middleware/requireAuth.ts
@@ -6,7 +6,7 @@ export async function requireAuth(req: any, res: any, next: any) {
   });
 
   if (!session) {
-    return res.status(401).json({ error: "Unauthorized" });
+    return res.status(401).json({success:false, message: "Access to API currently unauthorized use website to login" });
   }
 
   req.user = session.user;
